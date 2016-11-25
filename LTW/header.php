dc1282498e50723fc987ae03f5b9c0c42bd0ccaa
teresa-Guilherme <francisco_c_gomes@hotmail.com>
2016-11-25 18:24:29 +0000

    Delete header.php

--- LTW/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Adoro Comer</title>
-	<meta charset='UTF-8'>
-	<link rel="stylesheet" href="style.css">	
-	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-</head>
-<body>
