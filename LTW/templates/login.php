401d1cc835c252c50cce7fd1e83dfee16ddbe402
teresa-Guilherme <francisco_c_gomes@hotmail.com>
2016-12-05 03:13:37 +0000

    Update login.php

--- LTW/templates/login.php
+++ LTW/templates/login.php
@@ -1,11 +1,12 @@
-<div id="login">
-	<form>
-		<fieldset>
-			Username:<input type='text' id='username'><br>
-			Password:<input type='password' id='password'><br>
-			<button id='submit' type='reset'>Log in</button>
-			<button id='cancel'>Cancel</button>
-		</fieldset>
-	</form>
+<div id='fundo'>
+	<div id="login">
+			<img src="images/site/login.png" alt="loginImage"><br>
+			<label><b>Username</b></label><br>
+			<input type='text' placeholder="Enter Username" id='username'><br>
+			<label><b>Password</b></label><br>
+			<input type='password' placeholder="Enter Password" id='password'><br>
+			<button id='submit' type='button'>Log in</button> <br>
+			<button id='cancel' type='button'>Cancel</button>
+	</div>
 </div>
 <script src="scripts/login.js" type="text/javascript"></script>
